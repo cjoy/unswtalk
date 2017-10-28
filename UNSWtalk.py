49231064b5653deed8ed52bf98c718cf2e776fad
Chris Joy <chrisjoydev@gmail.com>
2017-10-28 22:19:19 +1100

    subset 1 finished

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -4,24 +4,96 @@
 # as a starting point for COMP[29]041 assignment 2
 # https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
 
-import os
-from flask import Flask, render_template, session
-
-students_dir = "dataset-small";
-
+import os, re
+from flask import Flask, render_template, session, request, redirect, url_for
 import controller as ctrl
+
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET','POST'])
-@app.route('/start', methods=['GET','POST'])
 def start():
-    n = session.get('n', 0)
-    students = sorted(os.listdir(students_dir))
-    student_to_show = students[n % len(students)]
+    # protected route - check if user logged in
+    if 'logged_in' not in session:
+        return redirect('/login')
+
+    return redirect('/feeds')
+
+
+
+@app.route('/search/people', methods=['GET','POST'])
+def search_people():
+    # protected route - check if user logged in
+    if 'logged_in' not in session:
+        return redirect('/login')
+
+    # get search query
+    query = request.args.get('q')
+    if query == None:
+        query = ''
+
+    # compute search results
+    results = ctrl.SearchPeople(query)
+
+    return render_template('search_people.html', title='Search People - "' + query + '"', 
+        GetProfilePic=ctrl.GetProfilePic,
+        results=results, query=query)
+    
+
+@app.route('/profile/<zid>', methods=['GET','POST'])
+def profile(zid):
+    # protected route - check if user logged in
+    if 'logged_in' not in session:
+        return redirect('/login')
+        
+    student_to_show = zid
     details = ctrl.GetUserDetails(student_to_show)
-    print(ctrl.GetUserPosts(student_to_show))
-    session['n'] = n + 1
-    return render_template('start.html', student_details=details)
+    posts = ctrl.GetUserPosts(student_to_show)
+
+    courses = list(re.sub(r'(\(|\))', '', details['courses']).split(','))
+    friends = list(re.sub(r'(\(|\)|\s)', '', details['friends']).split(','))
+
+    return render_template('profile.html', title=details['full_name'],
+        student_details=details, posts=reversed(posts),
+        getdetails=ctrl.GetUserDetails, GetProfilePic=ctrl.GetProfilePic,
+        parseTime=ctrl.parseTime,
+        courses=courses, friends=friends)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+
+    # error status is set to true
+    status = True
+
+    zid = request.form.get('zid')
+    password = request.form.get('password')
+
+    # check zid and password and set session
+    if zid != None and password != None:
+        if ctrl.GetUserDetails(zid)['password'] == password:
+            session['logged_in'] = True
+            session['zid'] = zid
+            session['user_details'] = ctrl.GetUserDetails(zid)
+            return redirect('/profile/'+zid)
+        else:
+            status = False
+
+    return render_template('login.html', title="Login", status=status)
+
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    return render_template('register.html', title="Register")
+
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    session.pop('logged_in', None)
+    session.pop('zid', None)
+    session.pop('user_details', None)
+    return redirect('/login')
+
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
--- controller.py
+++ controller.py
@@ -1,10 +1,33 @@
 import os
 import glob
 import yaml, json, sys
+import re
+from datetime import datetime
 
-students_dir = "dataset-small";
+students_dir = "static/dataset-small";
 
-# helper functions
+
+# allow quotes in yaml files
+def literalFile(content):
+    content = re.sub('"','\\"',content)
+    return content
+
+# TODO: parse datatime (for posts etc)
+def parseTime(tstamp):
+    return tstamp
+
+# get user image
+def GetProfilePic(zid):
+    file_path = os.path.join(students_dir, zid, "img.jpg")
+    profile_pic = 'http://www.freeiconspng.com/uploads/profile-icon-9.png'
+    if os.path.exists(file_path):
+        profile_pic = file_path
+    # print(profile_pic)
+    return profile_pic
+
+
+
+# Get user details, given zid
 def GetUserDetails(zid):
     details_filename = os.path.join(students_dir, zid, "student.txt")
     details = {}
@@ -12,18 +35,70 @@ def GetUserDetails(zid):
         details = yaml.load(f.read())
     return details
 
+
+# Get all users (array of user objects returned)
+def GetAllUsers():
+    users = []
+    for user in sorted(os.listdir(students_dir)):
+        users.append(GetUserDetails(user))
+    return users
+
+
+# Search for people
+def SearchPeople(query):
+    results = []
+    for user in GetAllUsers():
+        if query.lower() in user['full_name'].lower():
+            results.append(user)
+    
+    return results
+
+
+# Get user posts, comments to those posts and replies to those posts
 def GetUserPosts(zid):
+    print("Getting posts for", zid)
     posts = []
     user_dir = os.path.join(students_dir, zid)
     post_files = glob.glob(user_dir+'/[0-9].txt')
+    # loop through posts
     for post in post_files:
-        print(post)
-        reply_files = glob.glob(post.split('.txt')[0]+'-[0-9].txt')
-        for reply in reply_files:
-            print(" ", reply)
-            reply_files = glob.glob(post.split('.txt')[0]+'-[0-9].txt')
+        post_file = open(post,'r')
+        post_content = yaml.safe_load(literalFile(post_file.read()))
+        post_file.close()
 
-        # with open(post) as f:
-        #     posts.append(yaml.load(f.read()))
+        comments_array = []
+        post_obj = {
+            'id': post,
+            'content': post_content,
+            'comments': comments_array
+        }
+        # loop through comments
+        comment_files = glob.glob(post.split('.txt')[0]+'-[0-9].txt')
+        for comment in comment_files:
+            comment_file = open(comment,'r')
+            comment_content = yaml.safe_load(literalFile(comment_file.read()))
+            comment_file.close()
+
+            replies_array = []
+            comment_obj = {
+                'id': comment,
+                'content': comment_content,
+                'replies': replies_array
+            }
+            # loop through replies
+            reply_files = glob.glob(comment.split('.txt')[0]+'-[0-9].txt')
+            for reply in reply_files:
+                reply_file = open(reply,'r')
+                reply_content = yaml.safe_load(literalFile(reply_file.read()))
+                reply_file.close()
+                reply_obj = {
+                    'id': reply,
+                    'content': reply_content,
+                }
+                replies_array.append(reply_obj)
+
+            comments_array.append(comment_obj)
+
+        posts.append(post_obj)
 
     return posts
